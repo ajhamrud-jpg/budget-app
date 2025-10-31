822755184ff5f0b3b05b55d29db061b8ebb17c5b
Verity Bennett <veriteabennett@gmail.com>
2025-10-31 22:41:07 +0000

    skeleton code added

---
+++ src/main/java/budget/app/Main.java
@@ -0,0 +1,11 @@
+package budget.app;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println(String.format("Welcome to our budgeting app!"));
+    }
+}
