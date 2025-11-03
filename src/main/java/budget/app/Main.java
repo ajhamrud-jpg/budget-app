6ae50b6fd41fbf7e6eccb39e648a50d342ed5ea7
Verity Bennett <veriteabennett@gmail.com>
2025-11-03 20:10:55 +0000

    adding categories

--- src/main/java/budget/app/Main.java
+++ src/main/java/budget/app/Main.java
@@ -1,11 +1,24 @@
 package budget.app;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.math.BigDecimal;
+import java.util.List;
+
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println(String.format("Welcome to our budgeting app!"));
+        System.out.println("Welcome to our budgeting app!");
+
+        BudgetService budgetService = new BudgetService();
+        System.out.println("Your categories are: %s.".formatted(budgetService.categoryList));
+
+        List<Category> requestCategories = List.of(
+                new Category("fooooooooD", new BigDecimal("100.00")));
+
+        if (budgetService.addCategories(requestCategories)){
+            System.out.println("Categories Added");
+            System.out.println("Your new categories are: %s.".formatted(budgetService.categoryList));
+        }
+
+
+
     }
 }
